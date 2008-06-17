cb355749f59db3ceee8b703fc2918eee48b8a295
Wouter Bolsterlee <wbolster@src.gnome.org>
2008-06-17 18:52:42 +0000

    (
    * The "break all things" commit. Urgh, autotools needs some
      serious loving before stuff starts to work again... :(
    
    svn path=/branches/import-from-bzr/; revision=51

---
+++ autogen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-specimen"
+REQUIRED_AUTOCONF_VERSION=2.53
+REQUIRED_AUTOMAKE_VERSION=1.7.2
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/src/specimenwindow.py) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install the gnome-common module and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
+    exit 1
+}
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
